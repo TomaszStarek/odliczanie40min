054768e5c0e3f83a3e016654a99c71798d044737
TomaszStarek <33632173+TomaszStarek@users.noreply.github.com>
2020-05-28 11:46:09 +0200

    Add files via upload

---
+++ Gazpar_odliczanie40min/WindowsFormsApp5/Form1.Designer.cs
@@ -0,0 +1,203 @@
+﻿namespace WindowsFormsApp5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.minutki = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sekundy = new System.Windows.Forms.TextBox();
+            this.numerwozka = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.godzina = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox2
+            // 
+            this.textBox2.AcceptsTab = true;
+            this.textBox2.Location = new System.Drawing.Point(23, 69);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.MaxLength = 10;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(196, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(18, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 29);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Zeskanuj Barcode";
+            // 
+            // minutki
+            // 
+            this.minutki.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.minutki.Location = new System.Drawing.Point(350, 159);
+            this.minutki.MaxLength = 7;
+            this.minutki.Name = "minutki";
+            this.minutki.ReadOnly = true;
+            this.minutki.Size = new System.Drawing.Size(114, 102);
+            this.minutki.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(389, 11);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 91);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Zamykanie aplikacji";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sekundy
+            // 
+            this.sekundy.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.sekundy.Location = new System.Drawing.Point(470, 159);
+            this.sekundy.MaxLength = 7;
+            this.sekundy.Name = "sekundy";
+            this.sekundy.ReadOnly = true;
+            this.sekundy.Size = new System.Drawing.Size(114, 102);
+            this.sekundy.TabIndex = 8;
+            // 
+            // numerwozka
+            // 
+            this.numerwozka.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numerwozka.Location = new System.Drawing.Point(23, 159);
+            this.numerwozka.MaxLength = 7;
+            this.numerwozka.Name = "numerwozka";
+            this.numerwozka.ReadOnly = true;
+            this.numerwozka.Size = new System.Drawing.Size(196, 102);
+            this.numerwozka.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(18, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(168, 29);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Numer wózka";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(238, 264);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 29);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "godziny";
+            // 
+            // godzina
+            // 
+            this.godzina.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.godzina.Location = new System.Drawing.Point(232, 159);
+            this.godzina.MaxLength = 7;
+            this.godzina.Name = "godzina";
+            this.godzina.ReadOnly = true;
+            this.godzina.Size = new System.Drawing.Size(114, 102);
+            this.godzina.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(364, 265);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "minuty";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(474, 264);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(108, 29);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "sekundy";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(627, 332);
+            this.ControlBox = false;
+            this.Controls.Add(this.numerwozka);
+            this.Controls.Add(this.sekundy);
+            this.Controls.Add(this.godzina);
+            this.Controls.Add(this.minutki);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Odliczanie";
+            this.TopMost = true;
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox minutki;
+        private System.Windows.Forms.TextBox sekundy;
+        private System.Windows.Forms.TextBox numerwozka;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox godzina;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Gazpar_odliczanie40min/WindowsFormsApp5/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using EasyModbus;
+using System.IO.Ports;
+using System.Threading;
+using WiringPi;
+
+
+namespace WindowsFormsApp5
+{
+
+
+    public partial class Form1 : Form
+    {
+        const int przekaznik = 15;
+
+
+        struct wozki
+        {
+            public string numer;
+            public DateTime data_start;
+            public DateTime data_koniec;
+            public string czas;
+            public bool flaga;
+        }
+        wozki[] arr = new wozki[10000];
+
+
+
+        // this will prevent cross-threading between the serial port
+        // received data thread & the display of that data on the central thread
+        private delegate void preventCrossThreading(string x);
+        private preventCrossThreading accessControlFromCentralThread;
+
+        public Form1()
+        {
+            InitializeComponent();
+            inicjalizacja();
+           // tworzeniepliku();
+            odczytywanko();
+            textBox2.Select();
+            textBox2.KeyDown += textBox2_KeyDown;
+             textBox2.PreviewKeyDown += textBox2_PreviewKeyDown;
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+
+
+            textBox2.AcceptsTab = true;
+
+          //  for (int i = 0; i < 20; i++)
+          //  {
+          //      arr[i].data_koniec = DateTime.Now;
+           //     arr[i].data_start = DateTime.MaxValue;
+           //     arr[i].czas = "0";
+          //     arr[i].flaga = false;
+          //  }
+
+
+        }
+
+        public static void OnApplicationExit(object sender, EventArgs e)
+        {
+
+            GPIO.pinMode(przekaznik, (int)GPIO.GPIOpinmode.Output);
+            GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.Low);
+
+
+        }
+
+        static void inicjalizacja()
+        {
+
+            //Place the LED on GPIO 5 (Physical Pin 29)
+
+
+
+            // Tell the user that we are attempting to start the GPIO
+            Console.WriteLine("Initializing GPIO Interface");
+
+            // The WiringPiSetup method is static and returns either true or false
+            // Any value less than 0 represents a failure
+
+            if (Init.WiringPiSetupPhys() >= 0)
+
+
+            //ensures that it initializes the GPIO interface and reports ready to work. We will use Physical Pin Numbers
+            if(true)
+            {
+                // Tell the Pi that we will send data out the GPIO
+                GPIO.pinMode(przekaznik, (int)GPIO.GPIOpinmode.Output);
+
+                //Ensure that the LED is OFF
+                //Remember the supply is 3.3V(high) therefore: High-High=0 --> LED is OFF
+                GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.Low);
+
+                // Tell the user that GPIO Initialization Completed successfully
+                Console.WriteLine("GPIO Initialization Complete");
+            }
+            else
+            {
+                //Tell the user that GPIO Interface did not initialize
+                Console.WriteLine("GPIO Initialization Failed!");
+            }
+        }
+
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //    int errorcode = 0;
+            //    port.Close();
+            //Application.Restart();
+            textBox2.Clear();
+            minutki.Clear();
+
+            //label1.Text = "Zeskanuj barcode";
+
+            //     Environment.Exit(errorcode);
+            textBox2.ReadOnly = false;
+            textBox2.Select();
+
+
+            for (int i = 0; i < 20; i++)
+            {
+                arr[i].data_koniec = DateTime.Now;
+                arr[i].data_start = DateTime.MaxValue;
+                arr[i].czas = "0";
+                arr[i].flaga = false;
+
+            }
+            //GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.Low);
+            //Application.Exit();
+            System.Environment.Exit(0);
+            // GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.Low);
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength > 2)
+                textBox2.Clear();
+
+
+        }
+
+
+        private void textBox2_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        {
+            // Determine whether the key entered is the F1 key. If it is, display Help.
+            if (e.KeyCode == Keys.Enter)
+            {
+                int licznik;
+                string kupa;
+                kupa = textBox2.Text;
+                tworzeniepliku();
+                //numerwozka.Text = kupa;
+                textBox2.Clear();
+
+                if (int.TryParse(kupa, out licznik))
+                {
+                    if (arr[licznik].flaga == false)
+                    {
+                        arr[licznik].flaga = true;
+                        wejscie(licznik);
+
+                    }
+                }
+                // Display a pop-up Help topic to assist the user.
+                //  Help.ShowPopup(textBox2, "Enter your name.", new Point(textBox2.Bottom, textBox2.Right));
+            }
+            if (e.KeyCode == Keys.Tab)
+            {
+
+                string pomocnicza;
+                //MessageBox.Show("Tab");
+                pomocnicza = textBox2.Text;
+                numerwozka.Text = pomocnicza;
+                textBox2.Clear();
+                odliczanko(pomocnicza);
+
+                // odliczanko(textBox2.Text);
+                // textBox2.Clear();
+                // Display a pop-up Help topic to provide additional assistance to the user.
+                // Help.ShowPopup(textBox2, "Enter your first name followed by your last name. Middle name is optional.",
+                //    new Point(textBox2.Top, this.textBox2.Left));
+            }
+        }
+           private void textBox2_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+           {
+              if (e.KeyData == Keys.Tab)
+              {
+
+
+
+                   e.IsInputKey = true;
+               }
+               if (e.KeyData == (Keys.Tab | Keys.Shift))
+               {
+                    //MessageBox.Show("Shift + Tab");
+                   e.IsInputKey = true;
+               }
+           }
+
+        private void wejscie(int licznik)
+        {
+            // int licznik;
+            // if (int.TryParse(sn, out licznik))
+            //  {
+            // arr[licznik].numer = licznik;
+            arr[licznik].data_start = DateTime.Now;
+            arr[licznik].flaga = true;
+            tworzeniepliku();
+
+            //MessageBox.Show("data startu dla:{0}", arr[licznik].numer);
+
+            //  }
+            //   else;//nie wiem co
+        }
+
+        private void odliczanko(string sn)
+        {
+
+            int licznik, minutki;
+            float minutkif;
+
+            if (int.TryParse(sn, out licznik))
+            {
+                // GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.High);
+                do
+                {
+                    //MessageBox.Show("zaczynam");
+                    arr[licznik].data_koniec = DateTime.Now;
+                    TimeSpan result = arr[licznik].data_koniec - arr[licznik].data_start;
+
+
+
+                    string Minutystring = result.TotalMinutes.ToString();
+
+                    string godiznkiwysietlanie = result.Hours.ToString();
+                    string minutkiwysietlanie = result.Minutes.ToString();
+                    string sekundystring = result.Seconds.ToString();
+                    float.TryParse(Minutystring, out minutkif);
+                    minutki = (int)minutkif;
+
+                    if (minutki < 0)
+                        arr[licznik].data_start = DateTime.Now;
+                    //  MessageBox.Show("bufor to:", minutkif);
+                    // MessageBox.Show("bufor to:", Minutystring);
+                    //  MessageBox.Show("inutki");
+                    //RCVbox.Clear();
+                    // Application.DoEvents(); //służy do obsługi zdarzeń poza pętlą (zmienia wartość label1 i obsługa przycisków)
+                    //Thread.Sleep(500);
+                    // odliczanko(sn);
+                    this.godzina.Text = godiznkiwysietlanie;
+                    this.minutki.Text = minutkiwysietlanie;
+                    this.sekundy.Text = sekundystring;
+                    numerwozka.Text = sn;
+
+                    //Thread.Sleep(250);
+
+                    //RCVbox.Clear();
+                    if (minutki < 40)
+                    {
+                        Application.DoEvents();
+                        this.BackColor = Color.Red;
+                        GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.High);
+                    }
+                    else
+                        this.BackColor = Color.Green;
+                } while (minutki < 40);
+
+
+                if (minutki >= 40)
+                {
+                    // port.Write("OK\r");
+                    GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.Low);
+
+
+                    // arr[licznik].data_koniec = arr[licznik].data_start = DateTime.MinValue;
+                }
+                else
+                {
+                    GPIO.digitalWrite(przekaznik, (int)GPIO.GPIOpinvalue.High);
+
+                }
+                arr[licznik].flaga = false;
+                tworzeniepliku();
+
+            }
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void tworzeniepliku()
+        {
+            string sciezka = ("/home/pi/Myapp");      //definiowanieścieżki do której zapisywane logi
+
+           if (Directory.Exists(sciezka))       //sprawdzanie czy  istnieje
+           {
+               ;
+            }
+            else
+                System.IO.Directory.CreateDirectory(sciezka); //jeśli nie to ją tworzy
+
+            using (StreamWriter sw = new StreamWriter("/home/pi/Myapp/" + "zapis.txt"))
+            {
+                for (int i = 0; i < 20; i++)
+                {
+
+                    //sw.WriteLine("licznik:{0},start:{1},flaga:{2}",i,arr[i].data_start, arr[i].flaga);
+                    sw.WriteLine(arr[i].data_start);
+                    sw.WriteLine(arr[i].flaga);
+
+
+
+                }
+                sw.Close();
+            }
+            
+        }
+
+
+        private void odczytywanko()
+        {
+            string sciezka = ("/home/pi/Myapp/" + "zapis.txt");
+
+            using (StreamReader sr = new StreamReader(sciezka))
+            {
+                int i = 0;
+                while (sr.Peek() >= 0)
+                {
+                    
+                    arr[i].data_start = Convert.ToDateTime(sr.ReadLine());
+                    arr[i].flaga = bool.Parse(sr.ReadLine());
+
+                    i++;
+                    //sw.WriteLine(arr[i].flaga);
+
+
+
+
+                }
+                sr.Close();
+            }
+        }
+
+
+
+    }
+}
